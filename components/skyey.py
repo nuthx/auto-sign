063cbc05c87238b7d734ea7cc7e47fe0c1548989
NuthX <huermos@gmail.com>
2023-09-05 12:44:56 +0800

    更新天雪字幕下载

--- components/skyey.py
+++ components/skyey.py
@@ -5,71 +5,75 @@ from bs4 import BeautifulSoup
 from components.function import *
 
 
-home_url = "https://www.skyey2.com/forum.php"
+home_url = "https://www.skyey2.com/"
 subtitle_url = "https://www.skyey2.com/forum.php?mod=forumdisplay&fid=75"
-download_url = "https://www.skyey2.com/download.php?type=zip&id=37068&name_mod=19"
+coin_url = "https://www.skyey2.com/home.php?mod=spacecp&ac=credit&showcredit=1"
 
 
-def skyey_sign():
+def skyey_download():
     headers = {
+        "content-type": "application/x-www-form-urlencoded",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
         "cookie": get_cookies("skyey"),
-        "connection": "Keep-Alive",
-        "x-requested-with": "XMLHttpRequest",
-        "referer": home_url,
-        "content-type": "application/x-www-form-urlencoded"
+        "referer": subtitle_url
+    }
+
+    headers_zip = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
+                  "application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Dnt": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "cookie": get_cookies("skyey"),
+        "Referer": subtitle_url
     }
 
     # 打开字幕区
-    print(f"[{logtime(0)}] {BLUE}天雪签到{RESET} - 开始")
+    print(f"[{logtime(0)}] {BLUE}天雪(1/4){RESET} - 字幕下载开始")
     response = requests.get(subtitle_url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # # 获取字幕区帖子列表
-    # result_posts = soup.select("tbody[id^='normalthread_'] .new a:nth-child(3)")
-    # posts = []
-    # for post in result_posts:
-    #     href = post.get("href")
-    #     full_url = home_url + href
-    #     posts.append(full_url)
-    #
-    # # 获取前三个帖子
-    # for post in posts[:3]:
-    #     response = requests.get(subtitle_url, headers=headers)
-    #     print(post)
+    # 获取字幕区帖子列表
+    result_posts = soup.select("tbody[id^='normalthread_'] .new a:nth-child(3)")
+    posts = []
+    for post in result_posts:
+        href = post.get("href")
+        full_url = home_url + href
+        posts.append(full_url)
 
-    response = requests.get(download_url, headers=headers)
-    with open("lol.zip", "wb") as file:
-        file.write(response.content)
-    print(response)
+    # 获取前三个帖子的字幕文件并模拟下载
+    i = 1
+    for post in posts[:3]:
+        response = requests.get(post, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        download_link = soup.select_one("#filelistn + div a").get("href")
+        download_link_full = home_url + download_link
 
+        # 模拟下载
+        requests.get(download_link_full, headers=headers_zip)
+        print(f"[{logtime(0)}] {BLUE}天雪(2/4){RESET} - 模拟下载第{i}个字幕")
+        time.sleep(2)
+        i += 1
 
-
-
-
-
-
-
-
-
-
-
+    # 获取当前积分数
+    response = requests.post(coin_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    credit = soup.select_one(".creditl .cl").text[:10].replace("金币:", "").strip()
+    print(f"[{logtime(0)}] {BLUE}天雪(3/4){RESET} - 当前拥有{credit}金币")
 
 
-    # coin = soup.select_one("#extcreditmenu").text.replace("Gold:", "").strip()
-    # print(coin)
-
-    # total_num = soup.select_one("div.bw0 table tr:nth-child(2) td:nth-child(2)").text
-    # cycle_num = soup.select_one("div.bw0 table tr:nth-child(2) td:nth-child(3)").text
-    # print(f"[{logtime(0)}] {YELLOW}Chiphell签到 - 累计签到{total_num}次，已循环{cycle_num}个周期{RESET}")
-
-
-def skyey_sign_timer():
+def skyey_download_timer():
     while True:
         # 间隔24小时以上
         random_time = random.randint(86400, 87000)
 
         # 开始签到
-        skyey_sign()
-        print(f"[{logtime(0)}] {BLUE}天雪签到{RESET} - 下次将于{logtime(random_time)}开始")
+        skyey_download()
+        print(f"[{logtime(0)}] {BLUE}天雪(4/4){RESET} - 下次将于{logtime(random_time)}再次下载")
         time.sleep(random_time)
