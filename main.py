942f20442d2ec41f5c81fad71651be0b5be20d35
NuthX <huermos@gmail.com>
2023-09-05 12:50:57 +0800

    循环thread

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
+import time
 import threading
 from components import tsdm, chiphell, skyey, sayhuahuo, siksj, skland
 
 
 if __name__ == '__main__':
-    # tsdm_sign_thread = threading.Thread(target=tsdm.tsdm_sign_timer)
-    # tsdm_sign_thread.start()
+    run_timer = [
+        tsdm.tsdm_sign_timer,
+        tsdm.tsdm_work_timer,
+        chiphell.chiphell_sign_timer,
+        skyey.skyey_download_timer,
+        sayhuahuo.sayhuahuo_sign_timer,
+        # siksj.siksj_sign_timer,
+        skland.skland_apcheck_timer
+    ]
 
-    # tsdm_work_thread = threading.Thread(target=tsdm.tsdm_work_timer)
-    # tsdm_work_thread.start()
-
-    # chiphell_sign_thread = threading.Thread(target=chiphell.chiphell_sign_timer)
-    # chiphell_sign_thread.start()
-
-    # skyey_sign_thread = threading.Thread(target=skyey.skyey_sign_timer)
-    # skyey_sign_thread.start()
-
-    # sayhuahuo_sign_thread = threading.Thread(target=sayhuahuo.sayhuahuo_sign_timer)
-    # sayhuahuo_sign_thread.start()
-
-    # siksj_sign_thread = threading.Thread(target=siksj.siksj_sign_timer)
-    # siksj_sign_thread.start()
-
-    skland_apcheck_thread = threading.Thread(target=skland.skland_apcheck_timer())
-    skland_apcheck_thread.start()
+    for timer in run_timer:
+        thread = threading.Thread(target=timer)
+        thread.start()
+        time.sleep(8)
