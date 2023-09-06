35ba4a4753945d2ae3ba388f68155e32a170ac3e
NuthX <huermos@gmail.com>
2023-09-06 14:09:47 +0800

    调整签到间隔，将天雪放在最后执行

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
         tsdm.tsdm_sign_timer,
         tsdm.tsdm_work_timer,
         chiphell.chiphell_sign_timer,
-        skyey.skyey_download_timer,
         sayhuahuo.sayhuahuo_sign_timer,
         siksj.siksj_sign_timer,
         skland.skland_sign_timer,
         skland.skland_apcheck_timer,
+        skyey.skyey_download_timer,
     ]
 
     for timer in run_timer:
         thread = threading.Thread(target=timer)
         thread.start()
-        time.sleep(8)
+        time.sleep(7)
