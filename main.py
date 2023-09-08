7f33f5e2f97296862f7235fb729bd6a98b4e3088
NuthX <huermos@gmail.com>
2023-09-08 14:39:26 +0800

    增加启动版本号输出

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import time
 import threading
+from components.function import *
 from components import tsdm, chiphell, skyey, sayhuahuo, siksj, skland
 
 
 if __name__ == '__main__':
+    version = "1.01"
+    auto_sign = '''
+                 __                         _           
+    ____ ___  __/ /_____              _____(_)___ _____ 
+   / __ `/ / / / __/ __ \   ______   / ___/ / __ `/ __ \\
+  / /_/ / /_/ / /_/ /_/ /  /_____/  (__  ) / /_/ / / / /
+  \__,_/\__,_/\__/\____/           /____/_/\__, /_/ /_/ 
+                                          /____/        
+    '''
+
+    for line in auto_sign.splitlines():
+        print(f"[{logtime(0)}] {line}")
+        time.sleep(0.05)
+
+    print(f"[{logtime(0)}] ———————————————————————————————————————————————")
+    print(f"[{logtime(0)}] 自动签到程序启动（ver.{version}）")
+    print(f"[{logtime(0)}] ———————————————————————————————————————————————")
+
     run_timer = [
         tsdm.tsdm_sign_timer,
         tsdm.tsdm_work_timer,
@@ -18,4 +37,4 @@ if __name__ == '__main__':
     for timer in run_timer:
         thread = threading.Thread(target=timer)
         thread.start()
-        time.sleep(7)
+        time.sleep(8)
