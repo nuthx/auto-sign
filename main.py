0aac97ef82093f356242ae9317be4b18c2e20935
NuthX <huermos@gmail.com>
2023-10-12 12:05:59 +0800

    开启所有签到

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     log("———————————————————————————————————————————————")
 
     run_timer = [
-        # tsdm.tsdm_sign_timer,
-        # tsdm.tsdm_work_timer,
-        # chiphell.chiphell_sign_timer,
-        # sayhuahuo.sayhuahuo_sign_timer,
-        # siksj.siksj_sign_timer,
-        # skland.skland_sign_timer,
-        # skyey.skyey_download_timer,
+        tsdm.tsdm_sign_timer,
+        tsdm.tsdm_work_timer,
+        chiphell.chiphell_sign_timer,
+        sayhuahuo.sayhuahuo_sign_timer,
+        siksj.siksj_sign_timer,
+        skland.skland_sign_timer,
+        skyey.skyey_download_timer,
         vcb.vcb_sign_timer,
     ]
 
