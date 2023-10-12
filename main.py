470cc6059faf1ded48be083364e8bf9549dde07a
NuthX <huermos@gmail.com>
2023-10-12 11:42:13 +0800

    新增VCB论坛签到

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import time
 import threading
 from components.function import *
-from components import tsdm, chiphell, skyey, sayhuahuo, siksj, skland
+from components import tsdm, chiphell, skyey, sayhuahuo, siksj, skland, vcb
 
 
 if __name__ == '__main__':
     log("———————————————————————————————————————————————")
-    log("自动签到启动 20231008")
+    log("自动签到启动 20231012")
     log("———————————————————————————————————————————————")
 
     run_timer = [
-        tsdm.tsdm_sign_timer,
-        tsdm.tsdm_work_timer,
-        chiphell.chiphell_sign_timer,
-        sayhuahuo.sayhuahuo_sign_timer,
-        siksj.siksj_sign_timer,
-        skland.skland_sign_timer,
-        skyey.skyey_download_timer,
+        # tsdm.tsdm_sign_timer,
+        # tsdm.tsdm_work_timer,
+        # chiphell.chiphell_sign_timer,
+        # sayhuahuo.sayhuahuo_sign_timer,
+        # siksj.siksj_sign_timer,
+        # skland.skland_sign_timer,
+        # skyey.skyey_download_timer,
+        vcb.vcb_sign_timer,
     ]
 
     for timer in run_timer:
