99020a708b38f7e029c72494793fefcb71444125
NuthX <huermos@gmail.com>
2024-02-18 12:51:02 +0800

    优化main.py结构

--- main.py
+++ main.py
@@ -21,18 +21,24 @@ if __name__ == '__main__':
     log("自动签到启动 20231214")
     log("———————————————————————————————————————")
 
+    # 访问
     chiphell = every_second(random.randint(86401, 86500), task.chiphell_visit)
     vcb = every_day(random_time("08:00"), task.vcb_visit)
 
+    # 打工
     tsdm_1 = every_second(random.randint(21601, 21700), task.tsdm_work)
 
+    # 签到
     tsdm_2 = every_day(random_time("08:10"), task.tsdm_sign)
     sayhuahuo = every_day(random_time("08:20"), task.sayhuahuo_sign)
 
-    # sksj = every_day(random_time("08:30"), task.sksj_sign)
+    # 签到Plus
+    sksj = every_day(random_time("08:30"), task.sksj_sign)
 
+    # 字幕下载
     skyey = every_day(random_time("08:40"), task.skyey_download)
 
+    # 游戏签到
     skland = every_day(random_time("08:50"), task.skland_sign)
 
     # 启动时执行一次所有任务
@@ -52,7 +58,7 @@ if __name__ == '__main__':
         tsdm_1.run_pending()
         tsdm_2.run_pending()
         sayhuahuo.run_pending()
-        # sksj.run_pending()
+        sksj.run_pending()
         skyey.run_pending()
         skland.run_pending()
         time.sleep(1)
