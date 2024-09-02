ec0103148ac8ec72e33c34178a7114cf5988f093
NuthX <huermos@gmail.com>
2024-09-02 16:19:22 +0800

    fix: 修复4ksj无法获取K币

--- src/general/forum_sign_plus.py
+++ src/general/forum_sign_plus.py
@@ -16,7 +16,7 @@ def do(forum):
 
     headers = {
         "content-type": "application/x-www-form-urlencoded",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
         "cookie": COOKIE,
         "referer": URL + "/qiandao.php"
     }
