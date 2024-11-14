4ab6a7c4c4bb47ad17a133434d57046f4c0e8164
NuthX <huermos@gmail.com>
2024-11-14 11:51:21 +0800

    chore: 删除单独任务脚本

--- task/sign_psnine.py
+++
@@ -1,9 +0,0 @@
-from src.cookie import get_cookie
-from src.special import psnine
-
-
-if __name__ == '__main__':
-    psnine.do({
-        "psnid": get_cookie("psnine", "psnid", main=False),
-        "shell": get_cookie("psnine", "shell", main=False)
-    })
--- task/sign_skland.py
+++
@@ -1,8 +0,0 @@
-from src.cookie import get_cookie
-from src.special import skland
-
-
-if __name__ == '__main__':
-    skland.sign({
-        "token": get_cookie("skland", "token", main=False)
-    })
